84fb84cd0a39b5abce7926e57d91cb7127b554ea
Hrvoje Janđel <hrjandje@vsite.hr>
2023-10-09 21:10:03 +0200

    h

--- app/app.cpp
+++ app/app.cpp
@@ -1,12 +1,23 @@
 #include "app.h"
 
 #include <format>
-
+#include <cmath>
 namespace vsite::oop::p1 {
 
-/*
-* function implementations
-*/
+	/*
+	* function implementations
+	*/
+
+	int f1(int a, int b) {
+		return pow(a, b);
+
+	bool f2(int a, int b);
+
+	int f3(int a, int b, int c, int d);
+	int e = 0;
+	e = a + b + c + d;
+	return e;
 
 
-} // namespace
+	} // namespace
+}
--- app/app.h
+++ app/app.h
@@ -8,6 +8,9 @@ namespace vsite::oop::p1
 /*  
 * function declarations
 */
+	int f1(int, int);
+	bool f2(int, int);
+	int f3(int, int, int, int);
 
 
 }
